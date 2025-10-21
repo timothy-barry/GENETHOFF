a9738a749040a8d9a9e754117a495cb4c472ec08
timothy-barry <tbarry@hsph.harvard.edu>
2025-10-21 17:43:39 -0400

    clean up work dir

--- spry-1620/launch.sh
+++ spry-1620/launch.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=logs/process_spry_1620_data_%j.out
 #SBATCH --error=logs/process_spry_1620_data_%j.err
 #SBATCH --time=4:00:00
-#SBATCH --mem=8G
+#SBATCH --mem=32G
 #SBATCH -c 12
 #SBATCH --partition short
 
@@ -11,6 +11,7 @@
 pipeline_dir=$HOME"/research_code/GENETHOFF/"
 work_dir=$HOME"/research_offsite/work"
 cp $pipeline_dir"spry-1620/configuration.yml" $work_dir
+rm -r $work_dir/"*"
 
 # load modules
 module load conda/miniforge3/24.11.3-0
@@ -18,7 +19,7 @@ module load conda/miniforge3/24.11.3-0
 conda activate crisprde-venv
 
 # run command
-snakemake -n \
+snakemake \
 -s $pipeline_dir"00-pipeline/genethOFF.snakemake" \
 -j 12 \
 --use-conda \
