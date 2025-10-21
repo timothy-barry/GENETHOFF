ec79b856427bd4980702f3d8ca7b9fe37b1d2cc9
Tim Barry <tbarry2@andrew.cmu.edu>
2025-10-21 11:01:13 -0400

    prep launch of processing pipeline

---
+++ spry-1620/launch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=process_spry_1620_data
+#SBATCH --output=logs/process_spry_1620_data_%j.out
+#SBATCH --error=logs/process_spry_1620_data_%j.err
+#SBATCH --time=4:00:00
+#SBATCH --mem=8G
+#SBATCH -c 6
+#SBATCH --partition short
+
+snakemake -s ../00-pipeline/genethOFF.snakemake \
+  -j 6 \
+  -k \
+  --use-conda \ 
+  --report-after-run --report runtime_report.html  \
+  -n
